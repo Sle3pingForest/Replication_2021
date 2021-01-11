ead14e187110392d3877328ed86e342926a49256
TRABELSI ILEF <43601911+islebartfelia@users.noreply.github.com>
2021-01-11 16:05:19 +0100

    Synchronizer

--- src/Synchronizer.java
+++ src/Synchronizer.java
@@ -1,24 +1,35 @@
+package com.mycompany.project.src;
+
+import java.util.List;
+
 
 
 public class Synchronizer {
-    public void synchronize(FileSystemfs1, FileSystemfs2)
+    public void synchronize(FileSystem fs1, FileSystem fs2)
     {
-        FileSystemrefCopy1 = fs1.getReference();
-        FileSystemrefCopy2 = fs2.getReference();
+        FileSystem refCopy1 = fs1.getReference();
+        FileSystem refCopy2 = fs2.getReference();
         List<String> dirtyPaths1 = computeDirty(refCopy1, fs1, "");
         List<String> dirtyPaths2 = computeDirty(refCopy2, fs2, "");
-        reconcile(fs1, dirtyPaths1, fs2, dirtyPaths2, "");}
+        reconcile(fs1, dirtyPaths1, fs2, dirtyPaths2, "");
+    }
     
-    
-    public void reconcile(FileSystemfs1, List<String> dirtyPaths1, FileSystemfs2, List<String> dirtyPaths2, String currentRelativePath) 
+    /**
+     *
+     * @param fs1
+     * @param dirtyPath
+     */
+    public void reconcile(FileSystem fs1, List<String> dirtyPaths1, FileSystem fs2, List<String> dirtyPaths2, String currentRelativePath) 
     {
     
     }
    
     
-    public List<String> computeDirty(FileSystemlastSync, FileSystemfs, String currentRelativePath) 
+    public List<String> computeDirty(FileSystem lastSync, FileSystem fs, String currentRelativePath) 
     {
-    
+      List<String> modify = null ;  
+       
+     return modify;
     
     }
 }
