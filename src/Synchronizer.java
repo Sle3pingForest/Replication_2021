c347b3d851c0d41ff4dcdd0229c543296a0e2dca
TRABELSI ILEF <43601911+islebartfelia@users.noreply.github.com>
2021-01-11 15:39:00 +0100

    Synchronizer

---
+++ src/Synchronizer.java
@@ -0,0 +1,26 @@
+
+
+public class Synchronizer {
+    public void synchronize(FileSystemfs1, FileSystemfs2)
+    {
+        FileSystemrefCopy1 = fs1.getReference();
+        FileSystemrefCopy2 = fs2.getReference();
+        List<String> dirtyPaths1 = computeDirty(refCopy1, fs1, "");
+        List<String> dirtyPaths2 = computeDirty(refCopy2, fs2, "");
+        reconcile(fs1, dirtyPaths1, fs2, dirtyPaths2, "");}
+    
+    
+    public void reconcile(FileSystemfs1, List<String> dirtyPaths1, FileSystemfs2, List<String> dirtyPaths2, String currentRelativePath) 
+    {
+    
+    }
+   
+    
+    public List<String> computeDirty(FileSystemlastSync, FileSystemfs, String currentRelativePath) 
+    {
+    
+    
+    }
+}
+
+
